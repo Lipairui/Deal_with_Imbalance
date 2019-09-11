c8e23753d2d9d6756623c300be400480ec129d81
Lipairui <953383269@qq.com>
2019-09-11 15:25:16 +0800

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -1,6 +1,51 @@
-from imblearn.over_sampling import SMOTE, ADASYN
-from imblearn.under_sampling import ClusterCentroids, RandomUnderSampler, NearMiss, EditedNearestNeighbours, AllKNN, NeighbourhoodCleaningRule
+from imblearn.over_sampling import SMOTE, ADASYN, RandomOverSampler
+from imblearn.under_sampling import ClusterCentroids, RandomUnderSampler, NearMiss, EditedNearestNeighbours, RepeatedEditedNearestNeighbours, AllKNN, NeighbourhoodCleaningRule, OneSidedSelection
 from imblearn.combine import SMOTEENN,SMOTETomek
 
-def down_sampling(method):
+def under_sampling(X,y,method):
+  if method=='ClusterCentroids':
+    model = ClusterCentroids()
+    X_resampled, y_resampled = model.fit_resample(X, y)
+  elif method=='RandomUnderSampler':
+    model = RandomUnderSampler()
+    X_resampled, y_resampled = model.fit_resample(X, y)  
+  elif method=='NearMiss':
+    model = NearMiss()
+    X_resampled, y_resampled = model.fit_resample(X, y)
+  elif method=='EditedNearestNeighbours':
+    model = EditedNearestNeighbours()
+    X_resampled, y_resampled = model.fit_resample(X, y)
+  elif method=='RepeatedEditedNearestNeighbours':
+    model = RepeatedEditedNearestNeighbours()
+    X_resampled, y_resampled = model.fit_resample(X, y)
+  elif method=='AllKNN':
+    model = AllKNN()
+    X_resampled, y_resampled = model.fit_resample(X, y)
+  elif method=='NeighbourhoodCleaningRule':
+    model = NeighbourhoodCleaningRule()
+    X_resampled, y_resampled = model.fit_resample(X, y)
+  elif method=='OneSidedSelection':
+    model = OneSidedSelection()
+    X_resampled, y_resampled = model.fit_resample(X, y)
+  return X_resampled, y_resampled
   
+def over_sampling(X,y,method):
+  if method=='SMOTE':
+    model = SMOTE()
+    X_resampled, y_resampled = model.fit_resample(X, y)
+  elif method=='ADASYN':
+    model = ADASYN()
+    X_resampled, y_resampled = model.fit_resample(X, y)  
+  elif method=='RandomOverSampler':
+    model = RandomOverSampler()
+    X_resampled, y_resampled = model.fit_resample(X, y) 
+  return X_resampled, y_resampled
+  
+def combine_sampling(X,y,method):
+  if method=='SMOTEENN':
+    model = SMOTE()
+    X_resampled, y_resampled = model.fit_resample(X, y)
+  elif method=='SMOTETomek':
+    model = SMOTETomek()
+    X_resampled, y_resampled = model.fit_resample(X, y)  
+  return X_resampled, y_resampled   
